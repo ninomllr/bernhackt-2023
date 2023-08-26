6bf71614fb3750ac69ef92f27fd077bb1f68188a
Nino Müller <n.mueller@substring.ch>
2023-08-26 09:16:49 +0200

    added first table example

---
+++ Client/Element.cs
@@ -0,0 +1,10 @@
+namespace thorstopper.Client;
+
+public class Element
+{
+	public int Number { get; set; }
+	public string Sign { get; set; }
+	public string Name { get; set; }
+	public int Position { get; set; }
+	public double Molar { get; set; }
+}
